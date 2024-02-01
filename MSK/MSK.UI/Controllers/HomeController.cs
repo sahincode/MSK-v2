ac48729c5dde47cc94260e29d615daccacd20e03
sahinismayil <ismayilovsahin714@gmail.com>
2024-02-02 01:09:53 +0400

    Initial commit

---
+++ MSK/MSK.UI/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using System.Diagnostics;
+
+namespace MSK.UI.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+
+        public HomeController()
+        {
+            
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        
+    }
+}
