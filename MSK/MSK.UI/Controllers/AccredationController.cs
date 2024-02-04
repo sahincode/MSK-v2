2f1abe8bae1b07d1fe02f47479ec4541c2e18804
sahinismayil <ismayilovsahin714@gmail.com>
2024-02-05 01:50:11 +0400

    Accredation index page created

---
+++ MSK/MSK.UI/Controllers/AccredationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MSK.UI.Controllers
+{
+    public class AccredationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
