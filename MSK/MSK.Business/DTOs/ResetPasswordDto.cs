2c29c68c0daae49a69e650635e65c48c3e247c75
sahinismayil <ismayilovsahin714@gmail.com>
2024-02-26 23:10:19 +0400

    Some bug fixed in layout style .

--- MSK/MSK.Business/DTOs/ResetPasswordDto.cs
+++ MSK/MSK.Business/DTOs/ResetPasswordDto.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FluentValidation;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -16,8 +17,21 @@ namespace MSK.Business.DTOs
         [DataType(DataType.Password)]
         public string NewPassword { get;set; }
         [DataType(DataType.Password)]
+        
         public string ConfirmNewPassword { get; set; }
         public bool Succeeded { get; set; }
 
+    }
+    public class ResetPasswordDtoValidator : AbstractValidator<ResetPasswordDto>
+    {
+        public ResetPasswordDtoValidator()
+        {
+            RuleFor(x => x.NewPassword)
+                .NotEmpty().WithMessage("Password is required")
+                .MinimumLength(6).WithMessage("Password must be at least 6 characters");
+
+            RuleFor(x => x.ConfirmNewPassword)
+                .Equal(x => x.NewPassword).WithMessage("Passwords do not match");
+        }
     }
 }
